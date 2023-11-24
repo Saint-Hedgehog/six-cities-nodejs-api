eb2cf6000c02c44e1c5e8f7ebff5088aaf6b05af
Vadim <svyatoiej@gmail.com>
2023-11-24 22:44:19 +0200

    3.9. Архитектура приложения

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -6,3 +6,4 @@ export * from './user.type.js';
 export * from './user-status-type.enum.js';
 export * from './rent-offer.type.js';
 export * from './mock-server-data.type.js';
+export * from './component.enum.js';
